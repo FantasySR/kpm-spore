f70ad1d2d013cec0e18ff587c3bce024c73aec5e
FantasySR <192044614@qq.com>
2026-04-28 01:18:51 +0800

    更新 module.c

--- modules/KernelMemorySky/module.c
+++ modules/KernelMemorySky/module.c
@@ -1,150 +1,119 @@
 #include <compiler.h>
 #include <kpmodule.h>
+#include <hook.h>
 #include <linux/printk.h>
-#include <linux/uaccess.h>
-#include <linux/errno.h>
-#include <linux/sched.h>
-#include <linux/sched/mm.h>
-#include <linux/mm.h>
-#include <linux/slab.h>
-#include <linux/gfp.h>
+#include <linux/kallsyms.h>
+#include <linux/uaccess.h>  // 为了一些类型定义
 
 KPM_NAME("KernelMemorySky");
 KPM_VERSION("1.0.0");
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("FantasySR");
-KPM_DESCRIPTION("Kernel memory r/w via KPM_CTL0");
+KPM_DESCRIPTION("System call interceptor for pread64/pwrite64/process_vm_readv/writev");
 
-/* 手动声明 KernelPatch 风格的内核函数指针 */
-extern typeof(find_task_by_vpid) *kf_find_task_by_vpid;
-extern typeof(get_task_mm)      *kf_get_task_mm;
-extern typeof(mmput)            *kf_mmput;
-extern typeof(access_process_vm) *kf_access_process_vm;
-extern typeof(copy_from_user)   *kf_copy_from_user;
-extern typeof(copy_to_user)     *kf_copy_to_user;
-extern typeof(kmalloc)          *kf_kmalloc;
-extern typeof(kfree)            *kf_kfree;
+/* ---- 系统调用地址 (动态查找) ---- */
+static uintptr_t addr_pread64;
+static uintptr_t addr_pwrite64;
+static uintptr_t addr_process_vm_readv;
+static uintptr_t addr_process_vm_writev;
 
-/* 将函数调用重定向到函数指针 */
-#define find_task_by_vpid   (*kf_find_task_by_vpid)
-#define get_task_mm         (*kf_get_task_mm)
-#define mmput               (*kf_mmput)
-#define access_process_vm   (*kf_access_process_vm)
-#define copy_from_user      (*kf_copy_from_user)
-#define copy_to_user        (*kf_copy_to_user)
-#define kmalloc             (*kf_kmalloc)
-#define kfree               (*kf_kfree)
-
-/* 缺失的宏 */
-#ifndef GFP_KERNEL
-#define GFP_KERNEL 0xcc0U
-#endif
-#ifndef FOLL_FORCE
-#define FOLL_FORCE 0x10
-#endif
-#ifndef FOLL_WRITE
-#define FOLL_WRITE 0x01
-#endif
-
-/* 命令定义 */
-#define CMD_READ_MEM  0x1001
-#define CMD_WRITE_MEM 0x1002
-
-struct mem_data {
-    pid_t pid;
-    unsigned long addr;
-    unsigned long size;
-    void __user *buf;
-};
-
-static long amf_ctl0(const char *args, char __user *out_msg, int outlen)
+/* ---- 记录拦截信息的回调 ---- */
+static void before_pread64(hook_fargs4_t *fargs, void *udata)
 {
-    unsigned int cmd;
-    struct mem_data __user *user_data;
-    struct mem_data data;
-    struct task_struct *task;
-    struct mm_struct *mm;
-    void *kbuf = NULL;
-    long ret = 0;
-    int bytes;
+    pid_t pid = current->pid;
+    unsigned int fd = (unsigned int)fargs->arg0;
+    void __user *buf = (void __user *)fargs->arg1;
+    size_t count = (size_t)fargs->arg2;
+    loff_t pos = (loff_t)fargs->arg3;
 
-    if (!args || outlen < sizeof(cmd))
-        return -EINVAL;
-
-    if (copy_from_user(&cmd, args, sizeof(cmd)))
-        return -EFAULT;
-
-    if (cmd != CMD_READ_MEM && cmd != CMD_WRITE_MEM)
-        return -ENOTTY;
-
-    user_data = (struct mem_data __user *)(args + sizeof(cmd));
-
-    if (copy_from_user(&data, user_data, sizeof(data)))
-        return -EFAULT;
-
-    if (data.size > 0x100000)
-        return -EINVAL;
-
-    task = find_task_by_vpid(data.pid);
-    if (!task)
-        return -ESRCH;
-
-    mm = get_task_mm(task);
-    if (!mm)
-        return -EINVAL;
-
-    kbuf = kmalloc(data.size, GFP_KERNEL);
-    if (!kbuf) {
-        mmput(mm);
-        return -ENOMEM;
-    }
-
-    switch (cmd) {
-    case CMD_READ_MEM:
-        bytes = access_process_vm(task, data.addr, kbuf, data.size, FOLL_FORCE);
-        if (bytes > 0) {
-            if (copy_to_user(data.buf, kbuf, bytes))
-                ret = -EFAULT;
-            else
-                ret = bytes;
-        } else if (bytes == 0) {
-            ret = 0;
-        } else {
-            ret = bytes;
-        }
-        break;
-
-    case CMD_WRITE_MEM:
-        if (copy_from_user(kbuf, data.buf, data.size)) {
-            ret = -EFAULT;
-            break;
-        }
-        bytes = access_process_vm(task, data.addr, kbuf, data.size,
-                                  FOLL_FORCE | FOLL_WRITE);
-        if (bytes >= 0)
-            ret = bytes;
-        else
-            ret = bytes;
-        break;
-    }
-
-    kfree(kbuf);
-    mmput(mm);
-    return ret;
+    printk(KERN_INFO "KMS| pread64 | PID=%d FD=%u BUF=%px COUNT=%zu POS=%lld\n",
+           pid, fd, buf, count, pos);
 }
 
+static void before_pwrite64(hook_fargs4_t *fargs, void *udata)
+{
+    pid_t pid = current->pid;
+    unsigned int fd = (unsigned int)fargs->arg0;
+    const void __user *buf = (const void __user *)fargs->arg1;
+    size_t count = (size_t)fargs->arg2;
+    loff_t pos = (loff_t)fargs->arg3;
+
+    printk(KERN_INFO "KMS| pwrite64 | PID=%d FD=%u BUF=%px COUNT=%zu POS=%lld\n",
+           pid, fd, buf, count, pos);
+}
+
+static void before_process_vm_readv(hook_fargs6_t *fargs, void *udata)
+{
+    pid_t pid = current->pid;
+    pid_t target_pid = (pid_t)fargs->arg0;
+    const struct iovec __user *local_iov = (const struct iovec __user *)fargs->arg1;
+    unsigned long liovcnt = (unsigned long)fargs->arg2;
+    const struct iovec __user *remote_iov = (const struct iovec __user *)fargs->arg3;
+    unsigned long riovcnt = (unsigned long)fargs->arg4;
+    unsigned long flags = (unsigned long)fargs->arg5;
+
+    printk(KERN_INFO "KMS| process_vm_readv | PID=%d TARGET=%d LOCAL_IOV=%px LIOVCNT=%lu REMOTE_IOV=%px RIOVCNT=%lu FLAGS=%lu\n",
+           pid, target_pid, local_iov, liovcnt, remote_iov, riovcnt, flags);
+}
+
+static void before_process_vm_writev(hook_fargs6_t *fargs, void *udata)
+{
+    pid_t pid = current->pid;
+    pid_t target_pid = (pid_t)fargs->arg0;
+    const struct iovec __user *local_iov = (const struct iovec __user *)fargs->arg1;
+    unsigned long liovcnt = (unsigned long)fargs->arg2;
+    const struct iovec __user *remote_iov = (const struct iovec __user *)fargs->arg3;
+    unsigned long riovcnt = (unsigned long)fargs->arg4;
+    unsigned long flags = (unsigned long)fargs->arg5;
+
+    printk(KERN_INFO "KMS| process_vm_writev | PID=%d TARGET=%d LOCAL_IOV=%px LIOVCNT=%lu REMOTE_IOV=%px RIOVCNT=%lu FLAGS=%lu\n",
+           pid, target_pid, local_iov, liovcnt, remote_iov, riovcnt, flags);
+}
+
+/* ---- 模块生命周期 ---- */
 static long my_init(const char *args, const char *event, void __user *reserved)
 {
-    printk(KERN_INFO "KernelMemorySky: loaded (CTL0 mode)\n");
+    hook_err_t err;
+
+    // 动态查找系统调用地址
+    addr_pread64 = (uintptr_t)kallsyms_lookup_name("sys_pread64");
+    addr_pwrite64 = (uintptr_t)kallsyms_lookup_name("sys_pwrite64");
+    addr_process_vm_readv = (uintptr_t)kallsyms_lookup_name("sys_process_vm_readv");
+    addr_process_vm_writev = (uintptr_t)kallsyms_lookup_name("sys_process_vm_writev");
+
+    if (!addr_pread64 || !addr_pwrite64 || !addr_process_vm_readv || !addr_process_vm_writev) {
+        printk(KERN_ERR "KernelMemorySky: failed to find syscall addresses\n");
+        return -1;
+    }
+
+    // 安装钩子 (fp_hook_wrap 自动调用原函数)
+    err = fp_hook_wrap4(addr_pread64, before_pread64, NULL, NULL);
+    if (err) { printk(KERN_ERR "KMS: hook pread64 failed %d\n", err); return err; }
+
+    err = fp_hook_wrap4(addr_pwrite64, before_pwrite64, NULL, NULL);
+    if (err) { printk(KERN_ERR "KMS: hook pwrite64 failed %d\n", err); return err; }
+
+    err = fp_hook_wrap6(addr_process_vm_readv, before_process_vm_readv, NULL, NULL);
+    if (err) { printk(KERN_ERR "KMS: hook process_vm_readv failed %d\n", err); return err; }
+
+    err = fp_hook_wrap6(addr_process_vm_writev, before_process_vm_writev, NULL, NULL);
+    if (err) { printk(KERN_ERR "KMS: hook process_vm_writev failed %d\n", err); return err; }
+
+    printk(KERN_INFO "KernelMemorySky: interceptor loaded successfully\n");
     return 0;
 }
 
 static long my_exit(void __user *reserved)
 {
-    printk(KERN_INFO "KernelMemorySky: unloaded\n");
+    // 卸载钩子
+    if (addr_pread64)            fp_hook_unwrap(addr_pread64, before_pread64, NULL);
+    if (addr_pwrite64)           fp_hook_unwrap(addr_pwrite64, before_pwrite64, NULL);
+    if (addr_process_vm_readv)   fp_hook_unwrap(addr_process_vm_readv, before_process_vm_readv, NULL);
+    if (addr_process_vm_writev)  fp_hook_unwrap(addr_process_vm_writev, before_process_vm_writev, NULL);
+
+    printk(KERN_INFO "KernelMemorySky: interceptor unloaded\n");
     return 0;
 }
 
 KPM_INIT(my_init);
 KPM_EXIT(my_exit);
-KPM_CTL0(amf_ctl0);
